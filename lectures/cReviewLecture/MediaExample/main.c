555b2eb006c1386add995ecee5a3830e8ad7d7c8
Jamey Fraser <jfrase09@uoguelph.ca>
2017-09-12 03:13:18 -0400

    Added a library reader

--- lectures/cReviewLecture/MediaExample/main.c
+++ lectures/cReviewLecture/MediaExample/main.c
@@ -3,10 +3,29 @@
 
 #include "media.h"
 
-/****** Casting from void *********************/
+/* These print statements should be in their own Media files 
+ * For demonstration purposes only they exist here in the main file.
+ * /
+
+/**
+ * Prints integer data, does the same things as printIntType
+ * but shows the steps for casting
+ * @param data [description]
+ */
+void printIntSteps( void * data )
+{
+    // Cast void pointer to integer pointer
+    int* intPtr = (int*)data;
+    //Dereference the value
+    int value = *intPtr;
+    printf( "INTDATA %d\n", value );
+}
+
+
 void printIntType( void * data)
 {
-    printf( "INT MEDIA DATA %d\n", *((int*)mediaData) );
+    // Same as printIntSteps but in 1 step
+    printf( "INT MEDIA DATA %d\n", *((int*)data) );
 }
 
 void printDoubleType( void * data  )
@@ -34,29 +53,14 @@ int main( int argc, char ** argv )
     setMedia( &myLibrary[1], dptr);
 
     // What about getting our information back.
-    // Everything is now in a void pointer
 
-    // Now we have the ugly business of casting, lets do it in steps
-    // Get the media back
-    void* mediaData = getMedia( &myLibrary[0] ); 
-    // Convert it to int*, we know its int* only because we put it there.
-    int* tmpPtr = (int*)mediaData; 
-    // Dereference (Get the value) from the pointer which is an integer.
-    int value = *tmpPtr;
-    printf( "1) The first value in the library is %d\n", value );
+    // Printing using different function pointers
+    printMedia( getMedia(&myLibrary[0]), printIntSteps);
+    printMedia( getMedia(&myLibrary[0]), printIntType);
+    printMedia( getMedia(&myLibrary[1]), printDoubleType);
 
-    // Or do all the casting together in an ugly mess
-    printf( "2) The first value in the library is %d\n", *((int*)mediaData) );
-
-
-    // Printing our double
-    mediaData = getMedia( &myLibrary[1] );
-    printf( "The second value in the library is %f\n", *( (double*) mediaData ) );
-
-
-    // Combination of tools used 
-    printMedia( getMedia(&myLibrary[0], printIntType) );
-    printMedia( getMedia(&myLibrary[1], printDoubleType) );
+    free(myLibrary);
+    free(dptr);
 
     return 0;
 }
--- lectures/cReviewLecture/MediaExample/media.h
+++ lectures/cReviewLecture/MediaExample/media.h
@@ -1,10 +1,11 @@
 #ifndef MEDIA_H
 #define MEDIA_H
 
-
-
 /**
  * A structure to store media
+ * This is just a void data, we don't actually need a structure here
+ * But it creates an interface and an expectation.
+ * It Improves readability since Media is more specific than void
  */
 typedef struct media
 {
@@ -25,7 +26,14 @@ void setMedia( Media * media, void * data );
  */
 void* getMedia( Media * media );
 
+// Creating a function Pointer
 typedef void (*PrintMediaType)(void*);
+
+/**
+ * Print a media item
+ * @param media Data to be print
+ * @param print function pointer that can print that type of data.
+ */
 void printMedia( Media* media, PrintMediaType print);
 
 #endif
